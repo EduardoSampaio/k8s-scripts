75c96d6dbe3d0279944237c2a9499e07d7d5b3f7
Eduardo <edwardviana@gmail.com>
2025-06-27 21:48:11 -0300

    Update docs

--- fullcycle/server.go
+++ fullcycle/server.go
@@ -1,10 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
+		name := os.Getenv("NAME")
+		fmt.Fprintf(w, "Hello World %s", name)
 	})
+
 	http.ListenAndServe(":80", nil)
+	fmt.Println("Server running on port 80")
+
 }
